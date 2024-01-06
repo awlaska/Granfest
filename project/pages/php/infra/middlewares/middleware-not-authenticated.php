63d41ccd8d5233885113dc7a5f306e3a71b33bad
ruvensix <ruvensix@gmail.com>
2024-01-06 16:49:12 +0000

    remove prof

--- project/pages/php/infra/middlewares/middleware-not-authenticated.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../../helpers/session.php';
-
-if (isset($_SESSION['id']) || isset($_COOKIE['id'])) {
-    $home_url = 'http://' . $_SERVER['HTTP_HOST'] . 'project/pages/php/pages/secure';
-    header('Location: ' . $home_url);
-}
