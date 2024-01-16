9b15a8c30873a57cde9ec096f2e7fc0761923f8f
awlaska <79571817+awlaska@users.noreply.github.com>
2024-01-16 21:56:18 +0000

    Update dbh.inc.php

--- project/db/dbh.inc.php
+++ project/db/dbh.inc.php
@@ -1,12 +1,15 @@
 <?php 
-$host = 'localhost';
-$dbname = 'granfest';
-$dbusername = 'root';
-$dbpassword = '';
+$host = 'localhost'; # O host onde a BD está localizada
+$dbname = 'granfest'; # O nome da BD
+$dbusername = 'root'; # O user que será usado para entrar na BD
+$dbpassword = ''; # A password que será usada para entrar na BD
 
 try{
+    # String de conexão PDO que contém as informações sobre o acesso à BD
     $dsn = "mysql:host=$host; port= 3308; dbname=$dbname;charset=utf8";
+    # Cria uma nova instância da classe PDO
     $pdo = new PDO($dsn, $dbusername, $dbpassword);    
+    # Configura o modo de tratamento de erros
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch(PDOException $e){
